d576f1c90fc35134dff1f72fddb5975cf8e697d2
priyanksatani0208 <priyanksatani1234@gmail.com>
2022-07-28 17:55:54 +0530

    messa

---
+++ php_project_2023/web-1/sing_up.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sing up</title>
+    <link rel="stylesheet" type="text/css" href="signup.css">
+</head>
+<body>
+    <form>
+        <h1>Sing Up</h1>
+        <input type="text" name="fname" placeholder="Full Name">
+        <input type="number" name="pnumber" placeholder="Mobile Number ">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="Password" placeholder="Password">
+        <input type="password" name="r_Password" placeholder=" Repet Password">
+        <input type="submit" value="Sign Up" id="button">
+
+        
+    </form>
+</body>
+</html>
